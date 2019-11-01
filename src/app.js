4ec74c656d10883fe57bc89be9bfb8ac106b6b2b
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-11-01 09:16:23 -0600

    validateBearerToken working

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, API_TOKEN } = require('./config');
+const bookmarks = require('../store');
 
 const app = express();
 
@@ -13,10 +14,30 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(function validateBearerToken(req, res, next){
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[0] !== 'Bearer' || authToken.split(' ')[1] !== API_TOKEN) {
+    return res
+      .status(400)
+      .json({error: 'Invalid Authorization'});
+  } 
+  next();
+});
 
 app.get('/', (req,res) => {
-  return res.send('Hello, world!');
+  return res.send(bookmarks);
 });
+
+
+
+
+
+
+
+
+
+
+
 
 app.use(function errorHandler(error, req, res, next ){
   let response;
---
+++ store.js
@@ -0,0 +1,18 @@
+const bookmarks = [{
+  id:1,
+  title:'Google',
+  url: 'http://www.google.com',
+  description: 'google stuff',
+  rating: '4'
+},
+{
+  id:2,
+  title:'Imgur',
+  url: 'http://www.imgur.com',
+  description: 'imgur stuff',
+  rating: '5'
+},
+];
+
+
+module.exports = bookmarks;
