031a670f60ba60b480866af6443b37e8d5f5e8ba
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-11-01 14:54:31 -0600

    fixed store dummy items

--- src/store.js
+++ src/store.js
@@ -2,14 +2,14 @@ const bookmarks = [{
   id:'1',
   title:'Google',
   url: 'http://www.google.com',
-  description: 'google stuff',
+  desc: 'google stuff',
   rating: 4
 },
 {
   id:'2',
   title:'Imgur',
   url: 'http://www.imgur.com',
-  description: 'imgur stuff',
+  desc: 'imgur stuff',
   rating: 5
 },
 ];
